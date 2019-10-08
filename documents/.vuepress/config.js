dbd4f8ada1dc1e2a3f07dcd81899f2f825afe6a5
TK Sun <1037305420@qq.com>
2019-10-09 01:40:30 +0800

    fix path error

--- documents/.vuepress/config.js
+++ documents/.vuepress/config.js
@@ -4,7 +4,8 @@ module.exports = ctx => ({
   title: "Happy Coding",
   description: "苦海无涯,学无止境",
   port: 83,
-  dest: 'docs',
+  dest: './docs',
+  base: '/Blog/',
   plugins: [ [autobar, {pinyinNav: false}] ],
   themeConfig: {}
 });
