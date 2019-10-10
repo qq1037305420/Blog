5e75c995540d6369621fba6201223a61d5d63284
TKSun <1037305420@qq.com>
2019-10-10 19:40:21 +0800

    change dir name

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = ctx => ({
   title: "Happy Coding",
   description: "学无止境...",
   port: 83,
-  dest: './docs',
+  dest: './documents',
   base: '/Blog/',
   plugins: [ [autobar, {pinyinNav: false}] ],
   themeConfig: {}
